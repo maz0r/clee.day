c7edbd7f94d08442d4dc135c653c9843fe7ff3d2
maz0r <maz@maz.sh>
2025-05-24 15:30:19 +0100

    fixed time offset to be 07:00:00 UTC instead of 00:00:00 local
    changed from date to date.utc so the correct time is shown internationally rather than being dependent on local area codes.

--- clee_count.js
+++ clee_count.js
@@ -8,8 +8,9 @@ function updateTimeCounter() {
     if (currentDate >= new Date(currentYear,targetMonth,targetDay,23,59,59,999)){
         targetYear =  currentYear +1;
     }
-
-    const targetDateTime = new Date(targetYear, targetMonth, targetDay);
+  
+    //const targetDateTime = new Date(targetYear, targetMonth, targetDay);
+    const targetDateTime = new Date(Date.UTC(targetYear, targetMonth, targetDay, 7, 0, 0, 0)) // set to 07:00 utc
     const timeDifference = targetDateTime - currentDate;
     
     const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
@@ -23,7 +24,7 @@ function updateTimeCounter() {
     const yearElement = document.getElementById("time-year");
     if (
       (currentDate.getMonth() === targetMonth && currentDate.getDate() === targetDay) ||
-      (currentDate > targetDateTime && currentDate <= new Date(currentYear, targetMonth, targetDay, 23, 59, 59, 999))
+      (currentDate > targetDateTime && currentDate <= new Date(currentYear, targetMonth, targetDay+1, 06, 59, 59, 999))
     ) {
       yearElement.innerHTML = `clee day - ` + targetDateTime.getFullYear();
       timeElement.innerHTML = "is TODAY!!!!!!";
